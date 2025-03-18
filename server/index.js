0511a4e9cbf8a9d5bbe15a48b4a929d59b088afe
HallowsYves <83028938+HallowsYves@users.noreply.github.com>
2025-03-18 16:29:46 -0700

    added status to messages

--- server/index.js
+++ server/index.js
@@ -16,8 +16,6 @@ const expressServer = app.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`)
 })
 
-
-
 const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
@@ -27,8 +25,26 @@ const io = new Server(expressServer, {
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
+    // After connecting, send a message only to the user
+    // socket goes to user, IO goes to everyone
+    socket.emit('message', "Welcome to SecureChat!")
+
+    // announce who joined
+    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`)
+
+    // Listening for message event
+
     socket.on('message', data => {
         console.log(data)
         io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    // Announce user disconnection
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected.)`)
+    })
+
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 })

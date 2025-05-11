5ec54d8d3fb031c79e5884a68b35401e64a32c9a
Yves Velasquez <vyves@csu.fullerton.edu>
2025-05-11 00:10:56 -0700

    changed, storage bucket url

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBMM9I-7dL77Jd-GNaCGk9KCuW73eVkkxs",
   authDomain: "chat-95258.firebaseapp.com",
   projectId: "chat-95258",
-  storageBucket: "chat-95258.appspot.com",
+  storageBucket: "chat-95258.firebasestorage.app",
   messagingSenderId: "341631280166",
   appId: "1:341631280166:web:c7020a040bdfe54fc672e7"
 };
